6b0fe4e3c0a29ed46567438cadcd93b68b32037d
mo k <mo@mokhan.ca>
2012-08-25 18:36:50 -0600

    add omniauth-github gem and matching routes.

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ end
 
 gem 'jquery-rails'
 gem 'bootswatch-rails'
+gem 'omniauth-github'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Techtalk::Application.routes.draw do
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/auth/failure" => "sessions#failure"
+  match "signout" => "sessions#destroy", :as => :signout
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
